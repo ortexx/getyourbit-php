9c96e6d3307af7ed21b6bdd143aebb0b8a47c51e
Alexandr Balasyan <mywebstreet@gmail.com>
2017-08-12 14:39:59 +0300

    init

---
+++ README.md
@@ -0,0 +1,62 @@
+# [GetYourBit.com](https://getyourbit.com) client 
+
+This library allows you to make requests easily.
+
+## Examples
+
+```php
+require 'vendor/autoload.php';    
+
+use GetYourBit\Api;
+
+// create an instance
+$api = new Api('https://ip.getyourbit.com/');
+
+// login
+$api->auth('login', 'password');
+
+// request without scrolling
+$body = $api->request("/lookup/8.8.8.8", array('locale': 'en-US'));
+var_dump($body['data']);
+
+// request with scrolling
+$result = $api->scroll("/find/", array(
+    'size': 500,
+    'query' => array(
+        'country' => 'nepal'
+    )
+));
+echo count($result);
+
+//logout
+$api->logout();
+```
+
+## Api
+### .auth($login, $password, options=array())
+Login to the API. You can get __$login__ and __$password__ [on the site](https://getyourbit.com) after a subscription.  
+__options__ is [requests](https://github.com/rmccue/Requests) module options. 
+You can also pass __headers__ as option  
+Free services don't require authorization.
+
+### .logout()
+Logout from the API. It gives an error without authorization before.
+
+### .request($url, $data=array(), options=array())
+### .request($url, $options=array('data'=>array()))
+Request to the API without scrolling to get data.     
+It returns all response body as object.
+
+### .scroll($url, $data=array(), $options=array(), $callback=null)
+### .scroll($url, $data=array(), $callback=null)
+### .scroll($url, $callback=null)
+Request to the API with scrolling to get data. You can pass callback to control every chunk. You will get three arguments:
+
+* __$body__ - chunk response body
+* __$chunkData__ - chunk data
+* __$fullData__ - full data by the current chunk  
+
+It returns the full data at the end
+
+
+ 
---
+++ src/Api.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Getyourbit.com client
+ *
+ * @package GetYorBit
+ */
+
+namespace GetYourBit;
+
+use Requests;
+use \Exception;
+
+class Api {
+    public function __construct($url) {
+        $this->url = preg_replace('/[\/]+$/', '', $url);
+        $this->token = null;
+    }
+
+    protected function _request($url, $options = array()) {
+        $headers = array();
+        $data = array();
+
+        if(array_key_exists('headers', $options)) {
+             $headers = $options['headers'];
+             unset($options['headers']);
+        }
+
+        if(array_key_exists('data', $options)) {
+             $data = $options['data'];
+             unset($options['headers']);
+        }
+
+        $headers['Content-Type'] = 'application/json';
+        $response = Requests::post($url, $headers, json_encode($data), $options);
+        $body = json_decode($response->body, true);
+
+        if(isset($body['error'])) {
+            $message = $body['message'];
+
+            if(isset($ody['meta'])) {
+                $message = $message . ' ' . json_encode($body['meta']);
+            }    
+
+            throw new Exception($message);
+        }
+
+        return $body;
+    }
+
+    protected function next($res, $url, $options, $callback, $scroll) {
+        if($scroll) {
+            $options['data']['scroll'] = $scroll;
+        }
+
+        $body = $this->request($url, $options);
+        $res = array_merge($res, $body['data']);
+
+        if($callback) {
+            $callback($body, $body['data'], $res);
+        }
+
+        if(isset($body['scroll'])) {
+            return $this->next($res, $url, $options, $callback, $body['scroll']);
+        }
+
+        return $res;
+    }
+   
+    public function auth($user, $password, $options = array()) {
+        $options['auth'] = array($user, $password);
+        $body = $this->_request($this->url . '/auth/', $options);
+        $this->token = $body['token'];
+
+        return $this->token;
+    }
+
+    public function logout($options = array()) {
+        if(!$this->token) {
+            throw new Exception('You have to login before to logout');
+        }
+
+        $options['data'] = array('token' => $this->token);
+        $body = $this->_request($this->url . '/logout/', $options);
+        $this->token = null;
+
+        return $body;
+    }
+
+    public function request($url, $data = array(), $options = array()) {
+        if(func_num_args() == 2) {
+            $options = $data;
+        }
+
+        if(isset($options['data'])) {
+            $data = $options['data'];
+        }
+        else {
+            $data = array();
+        }
+
+        $data['token'] = $this->token;
+        $options['data'] = $data;
+        $url = $this->url . '/' . preg_replace('/^[\/]+/', '', $url);
+
+        return $this->_request($url, $options);
+    }
+
+    public function scroll($url, $data = array(), $options = array(), $callback = null) {        
+        if(is_callable($options)) {
+            $callback = $options;
+            $options = array();
+        }
+
+        if(is_callable($data)) {
+            $callback = $data;
+            $data = array();
+            $options = array();
+        }
+
+        if(!count($options)) {
+            $options = array();
+        }
+
+        $options['data'] = $data;
+        $res = array();
+        return $this->next($res, $url, $options, $callback, null);
+    }
+}
